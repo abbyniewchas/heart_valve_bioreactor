b75723940a65897aac5e203a8768bdf91935be21
Ted Desktop <lutkus.t@husky.neu.edu>
2020-01-12 20:50:16 -0500

    Sample actual pressure waveform included in test

--- Bioreactor Controls/waveform_test.m
+++ Bioreactor Controls/waveform_test.m
@@ -1,3 +1,11 @@
-time = [0.25 0.5 0.55];
-y = [50 0 -10];
+%% Sample for actual pressure waveform
+time = [0 0.25 0.4 1 1.1];
+y = [0 30 20 0 0];
 
+xx = 0:0.01:time(end);
+
+yy = pchip(time, y, xx);
+clf;
+plot(xx, yy)
+hold on
+plot(time, y, 'o')
