d054851f45be381e40997fddb2f5e0422c3cfa54
tmlut <tmlut@DESKTOP-V91F6OI>
2020-01-17 20:18:12 -0500

    finished lab_user_data excel sheet and pulling into gui

--- Bioreactor Controls/start_bioreactor.m
+++ Bioreactor Controls/start_bioreactor.m
@@ -7,10 +7,21 @@
 % Load lab user data from excel sheet
 [~,sheet_names] = xlsfinfo('gui_data/lab_user_settings.xlsx');
 for i = 1:length(sheet_names)
+    
+    % Load lab member's sheet to loop through experiments they have setup
     lab_sheet = readcell('gui_data/lab_user_settings.xlsx', 'Sheet', sheet_names{i});
     
-    lab_data.name
+    % Allocate memory to a struct to pass into the gui
+    lab_data.users(i) = sheet_names(i);
+    lab_data.experiments(i,:) = lab_sheet(1,2:end);
+    lab_data.peak_pressures(i,:) = lab_sheet(2,2:end);
+    lab_data.min_pressures(i,:) = lab_sheet(3,2:end);
+    lab_data.forward_pulse_time(i,:) = lab_sheet(4,2:end);
+    lab_data.backpressure_time(i,:) = lab_sheet(5,2:end);
+    lab_data.rest_time(i,:) = lab_sheet(6,2:end); %later bpm!!
+    lab_data.acq_freq(i,:) = lab_sheet(7,2:end);
+    lab_data.save_loc = lab_sheet(8,2:end);
 end
 
-
-%bioreactor_gui(lab_data);
+% Run the bioreactor and pass in the lab's data
+bioreactor_gui(lab_data);
--- Bioreactor Controls/waveform_test.m
+++ Bioreactor Controls/waveform_test.m
@@ -9,3 +9,19 @@ clf;
 plot(xx, yy)
 hold on
 plot(time, y, 'o')
+
+%% Waveform Test
+
+x = [0 0.2 0.6 0.65 0.8];
+y = [0 50 -10 0 0];
+
+xx = 0:0.001:x(end);
+
+yy = pchip(x, y, xx);
+
+clf;
+plot(x, y, 'o')
+hold on
+plot(xx, yy)
+legend({'data', 'pchip'})
+title('Waveform Sample')
